8e08110bc5a0afe9178b7d2ba86588cf643f9357
keomony khun <keomony@gmail.com>
2017-01-17 20:30:22 +0000

    creates XMLHttpRequest object

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// github user finder example
+$(document).ready(function() {
+  $(document).on('keypress', '#username', function() {
+    if (event.which === 13) { //check the key was <enter>
+      var input = $(this);
+      var username = input.val();
+      getGithubInfo(username);
+      // console.log('username was:' +username);
+    }
+  });
+});
+
+function getGithubInfo(username) {
+  var url= 'https://api.github.com/users/' +username;
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', url, false);
+  xmlhttp.send();
+
+  var data = xmlhttp.responseText;
+
+  console.log(data);
+}
